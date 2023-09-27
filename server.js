835666bed39c39fb7dd75f24c710cef0c01aa8e5
Maheep Gupta <91566211+CodingStuffsofMahee@users.noreply.github.com>
2023-09-28 03:07:41 +0530

    cors option removed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const corsOptions = {
   origin: 'https://mongo-dbloginblog-production.up.railway.app',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-app.use(cors(corsOptions))
+app.use(cors())
 app.use('/auth', LoginUser)
 app.get('/', (req,res) => {
   res.json({
